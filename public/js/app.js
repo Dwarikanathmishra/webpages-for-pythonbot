3ee6a8460b95b42626c0adcb0d1585935f34a23f
StrangeNoob <sipun63427452@gmail.com>
2019-09-16 00:39:04 +0530

    Add Backend and Nodejs

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB38JdAWJ17irTP18JmakWVTv_qnOVnQ1o",
+  authDomain: "shopicity-4d122.firebaseapp.com",
+  databaseURL: "https://shopicity-4d122.firebaseio.com",
+  projectId: "shopicity-4d122",
+  storageBucket: "shopicity-4d122.appspot.com",
+  messagingSenderId: "56465055257",
+  appId: "1:56465055257:web:2ddce704addef2fe279488"
+};
+firebase.initializeApp(firebaseConfig);
---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+http.createServer((req, res) => {
+  
+  console.log('The Server is Running in : http://127.0.0.1:3000/');
+  if(req.url == "/"){
+    fs.readFile("./public/index.html","UTF-8",function(err,html){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end(html);
+  });
+  } else if (req.url.match("\.css$")){
+      var csspath= path.join(__dirname,'public',req.url);
+      var filestream = fs.createReadStream(csspath,"UTF-8");
+      res.writeHead(200,{"Content-Type":"text/css"});
+      filestream.pipe(res);
+  } else if (req.url.match("\.js$")){
+    var jspath= path.join(__dirname,'public',req.url);
+    var filestream = fs.createReadStream(jspath,"UTF-8");
+    res.writeHead(200,{"Content-Type":"text/javascript"});
+    filestream.pipe(res);
+  }else if (req.url.match("\.html$")){
+    var htmlpath= path.join(__dirname,'public',req.url);
+    var filestream = fs.createReadStream(htmlpath,"UTF-8");
+    res.writeHead(200,{"Content-Type":"text/html"});
+    filestream.pipe(res);
+  }else if (req.url.match("\.jpg$")){
+    var htmlpath= path.join(__dirname,'public',req.url);
+    var filestream = fs.createReadStream(htmlpath,"UTF-8");
+    res.writeHead(200,{"Content-Type":"text/html"});
+    filestream.pipe(res);
+  }else{
+    res.writeHead(404,{"Content-Type":"text/html"});
+    res.end("No Page Found");
+  } 
+  
+}).listen(port);
